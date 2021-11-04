c9d135f353e6a3445840420578c92217c4320112
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-11-04 16:05:16 -0300

    adicionar métodos

--- src/main/java/br/com/zup/Modulo5_ExercicioInvestimento/controller/InvestimentoController.java
+++ src/main/java/br/com/zup/Modulo5_ExercicioInvestimento/controller/InvestimentoController.java
@@ -1,5 +1,28 @@
 package br.com.zup.Modulo5_ExercicioInvestimento.controller;
 
+import br.com.zup.Modulo5_ExercicioInvestimento.DTO.InvestimentoDTO;
+import br.com.zup.Modulo5_ExercicioInvestimento.DTO.RetornoDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/investimento")
+
 public class InvestimentoController {
 
+
+    @Autowired
+    private InvestimentoService simulacao;
+
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public RetornoDTO simularInvestimento(@RequestBody InvestimentoDTO investimentoDTO) {
+        //return  simulacao.(investimentoDTO);
+
+
+    }
 }
+
