6a00773dae90e1b874d1c08c4de825acf9ca1a74
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-11-04 16:06:52 -0300

    adicionar métodos a classe

--- src/main/java/br/com/zup/Modulo5_ExercicioInvestimento/DTO/RetornoDTO.java
+++ src/main/java/br/com/zup/Modulo5_ExercicioInvestimento/DTO/RetornoDTO.java
@@ -1,5 +1,35 @@
 package br.com.zup.Modulo5_ExercicioInvestimento.DTO;
 
 public class RetornoDTO {
+    private double valorInvestido;
+    private double valorTotalDolucro;
+    private double valorTotal;
+
+    public RetornoDTO() {
+    }
+
+    public double getValorInvestido() {
+        return valorInvestido;
+    }
+
+    public void setValorInvestido(double valorInvestido) {
+        this.valorInvestido = valorInvestido;
+    }
+
+    public double getValorTotalDolucro() {
+        return valorTotalDolucro;
+    }
+
+    public void setValorTotalDolucro(double valorTotalDolucro) {
+        this.valorTotalDolucro = valorTotalDolucro;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 
 }
