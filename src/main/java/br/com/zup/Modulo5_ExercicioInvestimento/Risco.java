180eda6e99ce6e2ca88b5123ebf31398553576ca
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-11-04 13:49:41 -0300

    criar enum

--- src/main/java/br/com/zup/Modulo5_ExercicioInvestimento/Risco.java
+++ src/main/java/br/com/zup/Modulo5_ExercicioInvestimento/Risco.java
@@ -1,6 +1,6 @@
 package br.com.zup.Modulo5_ExercicioInvestimento;
 
-public class Risco {
+public enum Risco {
     ALTO(2.5), MEDIO(0.0015), BAIXO(0.0005);
     private double taxaRetorno;
 
---
+++ src/main/java/br/com/zup/Modulo5_ExercicioInvestimento/config/MensagemErro.java
@@ -0,0 +1,2 @@
+package br.com.zup.Modulo5_ExercicioInvestimento.config;public class MensagemErro {
+}
