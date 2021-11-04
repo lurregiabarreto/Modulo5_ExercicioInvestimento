5cb2854053db91492c5a6003aabf43e7e1fb1fba
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-11-04 11:07:43 -0300

    primeiro commit

---
+++ src/main/java/br/com/zup/Modulo5_ExercicioInvestimento/Modulo5ExercicioInvestimentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.Modulo5_ExercicioInvestimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo5ExercicioInvestimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo5ExercicioInvestimentoApplication.class, args);
+	}
+
+}
